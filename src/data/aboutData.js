d59462dc197762aff75d1cf3c4f512c97c7152cc
Sapna190 <sapnasharma8849711@gmail.com>
2025-07-25 19:28:16 +0530

    updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "I'm a final-year Computer Science student at Uka Tarsadia University (AMTICS), Bardoli, Surat, passionate about front-end development. ",
+    "I'm a final-year Computer Science student at Uka Tarsadia University (AMTICS), Bardoli, Surat, passionate about Software Development",
   description2:
     "Currently seeking full-time and internship opportunities to apply my skills and knowledge. Eager to contribute to dynamic projects and learn from experienced professionals. Let's connect and create impactful digital experiences together!",
   image: 1,
